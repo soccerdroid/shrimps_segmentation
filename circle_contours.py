a8e4ed209119e108f97d609dc7a2cdde0f683cbc
soccerdroid <mguarand@espol.edu.ec>
2018-03-16 13:56:14 -0500

    Extended bounding box

--- circle_contours.py
+++ circle_contours.py
@@ -7,22 +7,26 @@ import bresenham
 
 """Open image in grayscale, apply bilateral filter and detect contours"""
 #img = cv2.imread('0000000002.tiff',0)
-img = cv2.imread('objects/object4.tiff',0)
+img = cv2.imread('objects/new_object4.tiff',0)
 
 #img = cv2.imread('../camaron_caja_negra.tif',0)
 rows,cols=img.shape
 
 #Threshold image
 bilateral_filtered_image = cv2.bilateralFilter(img, 7, 35, 150)
-ret,binary = cv2.threshold(bilateral_filtered_image,38,255,cv2.THRESH_BINARY)
-mask=np.zeros(img.shape)
+thresh =int(np.std(bilateral_filtered_image))
+ret,binary = cv2.threshold(bilateral_filtered_image,thresh,255,cv2.THRESH_BINARY)
+
+kernel = cv2.getStructuringElement(cv2.MORPH_CROSS,(3,3))
+binary = cv2.morphologyEx(binary, cv2.MORPH_CLOSE, kernel)
+cv2.imshow("agadf",binary)
+mask = np.zeros(img.shape)
 
 
 #Detected contours with cany, same output with just the binary image
 #edge_detected_image = cv2.Canny(binary, 75, 200)
-# cv2.imshow('Edge', edge_detected_image)
-# cv2.waitKey(0)
-#cv2.imwrite("edges.jpg",edge_detected_image)
+#cv2.imshow('Edge', edge_detected_image)
+cv2.waitKey(0)
 
 #Find contours of shrimps 
 contours= cv2.findContours(binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
@@ -34,7 +38,7 @@ for contour in contours:
     approx = cv2.approxPolyDP(contour,0.01*cv2.arcLength(contour,True),True)
     area = cv2.contourArea(contour)
     #just look for big objects
-    if ((len(approx) > 8) & (area > 50) ):
+    if ((len(approx) > 10) & (area > 100) ):
         contour_list.append(contour)
         #Draw minimun enclosing circle
         (x,y),radius = cv2.minEnclosingCircle(contour)
@@ -47,33 +51,30 @@ for contour in contours:
             #get segments of the ring delimitates by the external and internal circle that encloses the shrimps
             lineas=bresenham.circle_segmentation(img, center[0], center[1], radius)
             if(lineas!=-1):
-            	desviaciones=[]
-            	promedios=[]
+            	deviations=[]
+            	means=[]
                 for linea in lineas:
                     #get intensities from the coordinates of the segments
                     intensidades=img[linea[:,0],linea[:,1]]
-                    #plt.hist(intensidades.flatten(),256,[0,256], color = 'r')
-                    #plt.xlim([0,256])
-                    #plt.show()
                     #get standard deviation
-                    std=np.std(intensidades)
-                    mean=np.mean(intensidades)
+                    std = np.std(intensidades)
+                    mean = np.mean(intensidades)
                     #if(std>35 or std<22):
                     #if(mean>100 or mean<70):
                         #del lineas[count]
                     #else:
-                    desviaciones.append(std)
-                    promedios.append(mean)
-                    count+=1
+                    deviations.append(std)
+                    means.append(mean)
+                    count += 1
                     print(str(linea[0,0])+" , "+str(linea[-1,-1])+" -> "+str(std))
                 #for linea in lineas:
                     img[linea[:,0],linea[:,1]]=0
 
-                desviaciones=np.asarray(desviaciones)
-                promedios=np.asarray(promedios)
+                deviations=np.asarray(deviations)
+                means=np.asarray(means)
                 #ejex=np.arange(1,count+1)
-                plt.scatter(promedios, desviaciones)
-                plt.title('Desviaciones vs promedio de intensidades de segmentos')
+                plt.scatter(means, deviations)
+                plt.title('deviations vs promedio de intensidades de segmentos')
                 plt.show()
             #cv2.circle(mask,center,radius/2,(0,255,255),-1)
     		
--- shrimps_cropper.py
+++ shrimps_cropper.py
@@ -12,19 +12,25 @@ import matplotlib.pyplot as plt
 
 
 def binarize_otsu(img):
-	threshold = filter.threshold_otsu(img)
+	#threshold = filter.threshold_otsu(img)
+	threshold = np.std(img)
 	#print("asdfadsf " + str(threshold))
 	img_copy = img.copy()
 	img_copy[img_copy<threshold] = 0;
 	img_copy[img_copy>threshold] = 1;
 	return img_copy
 
-def transformObjects(objects):
+def transformObjects(objects, fils, cols):
 	t_objects = []
 	for o in objects:
 		slice1 = o[0]
 		slice2 = o[1]
-		t = (slice1.start,slice1.stop,slice2.start,slice2.stop)
+		fila_inicial = slice1.start - 20 if slice1.start - 20 >= 0 else 0 
+		fila_final = slice1.stop + 20 if slice1.stop + 20 <= fils else fils
+		col_inicial = slice2.start - 20 if slice2.start - 20 >= 0 else 0
+		col_final = slice2.stop + 20 if slice2.stop + 20 <= cols else cols
+		#t = (slice1.start,slice1.stop,slice2.start,slice2.stop) # (fila1,fila final, columna1, columna final) las filas y cols finales no se incluyen [)
+		t = (fila_inicial,fila_final,col_inicial,col_final)
 		t_objects.append(t)
 	return t_objects;
 
@@ -51,7 +57,7 @@ def saveObjects(boundingBoxes,input_img,output_dir):
 		area= (x2 - x)*(y2 - y)
 		if(area>17000):
 			crop_img = img[x:x2,y:y2]
-			name= "object"+str(count)+".tiff"
+			name= "new_object"+str(count)+".tiff"
 			path = join(output_dir,name)
 			io.imsave(path,crop_img)
 			count+=1
@@ -70,27 +76,28 @@ output_dir= args[1].strip()
 #Get image and binarize it
 img = io.imread(input_img)
 img_gray = rgb2gray(img)
+fils,cols=np.shape(img_gray)
 img=binarize_otsu(img_gray)
 
 #Label pixels with values different from 0
 label_img, num_labels = ndimage.label(input=img)
-print (label_img)
+
 io.imshow(img)
 plt.show()
 
-# Find bounding boxes of the objects in the image
-# objects = ndimage.find_objects(label_img);
+#Find bounding boxes of the objects in the image
+objects = ndimage.find_objects(label_img);
 
-# # se transforman los objectos
-# # a una estructura mas facil
-# objects = transformObjects(objects)
-# # se los ordena de acuerdo al area
-# objects = sorted(objects,compareObjects)
-# # los 7 objetos mas grandes son
-# # los bounding boxes de las pepas
-# #for o in objects:
-# 	#print(o[1] - o[0])*(o[3] - o[2])
-# boundingBoxes = objects[0:7]
+# se transforman los objectos
+# a una estructura mas facil
+objects = transformObjects(objects,fils,cols)
+# se los ordena de acuerdo al area
+objects = sorted(objects,compareObjects)
+# los 7 objetos mas grandes son
+# los bounding boxes de las pepas
+#for o in objects:
+	#print(o[1] - o[0])*(o[3] - o[2])
+boundingBoxes = objects[0:7]
 
-# #save an image per each shrimp
-# saveObjects(boundingBoxes,input_img,output_dir)
+#save an image per each shrimp
+saveObjects(boundingBoxes,input_img,output_dir)
